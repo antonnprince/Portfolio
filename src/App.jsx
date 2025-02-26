61c3e6c0c1a9e5e2a232f863d1e70d75c19236f0
antonprince <antonprince95@gmail.com>
2025-02-26 16:36:32 +0530

    hero begein

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Nav from './Nav'
-
+import Hero from './Hero'
 
 function App() {
 
@@ -9,11 +9,7 @@ function App() {
     className='flex flex-col p-4'
     >
       <Nav/>
-      <h1
-        className='text-3xl text-cyan-200 font-bold'
-      >
-        Welcome 
-      </h1>
+     <Hero/>
    </div>
   )
 }
