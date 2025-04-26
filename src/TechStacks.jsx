fd114170f86528a9f90203bc26093261d2a90c27
antonnprince <antonprince95@gmail.com>
2025-04-26 22:39:43 +0530

    hf add

--- src/TechStacks.jsx
+++ src/TechStacks.jsx
@@ -5,6 +5,8 @@ import express from "./assets/express.svg"
 import node from "./assets/node.svg"
 import firebase from "./assets/firebase.svg"
 import supabase from "./assets/supabase.svg"
+import fastapi from "./assets/fastapi.svg"
+import huggingface from "./assets/huggingface.svg"
 
 const TechStacks = () => {
   return (
@@ -16,15 +18,16 @@ const TechStacks = () => {
         className='my-12 mx-auto text-[#6B7280] text-2xl md:text-3xl lg:text-6xl font-extrabold'>
             TECH STACKS</h1>
 
-          {/* TO DO
-          Edit color from website itself */}
+       
             <div className='flex flex-row space-x-12'>
-              <img src={react} className='w-12 h-12 text-white'/>
-              <img src={mongo} className='w-12 h-12 text-white scale-250'/>
-              <img src={express} className='w-12 h-12 text-white'/>
-              <img src={node} className='w-12 h-12 text-white'/>
-              <img src={firebase} className='w-12 h-12 text-white'/>
-              <img src={supabase} className='w-12 h-12 text-white'/>
+              <img src={react} className='w-12 h-12 '/>
+              <img src={mongo} className='w-12 h-12  scale-250'/>
+              <img src={express} className='w-12 h-12 '/>
+              <img src={node} className='w-12 h-12 '/>
+              <img src={firebase} className='w-12 h-12 '/>
+              <img src={supabase} className='w-12 h-12 '/>
+              <img src={fastapi} className='w-12 h-12 '/>
+              <img src={huggingface} className='w-12 h-12 '/>
             </div>
     </div>
   )
