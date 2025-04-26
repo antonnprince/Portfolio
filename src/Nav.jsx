949462d39befaa84dcc8e8c70ff0647b71f24aa9
antonnprince <antonprince95@gmail.com>
2025-04-26 22:12:24 +0530

    nav resize

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -8,17 +8,17 @@ const Nav = () => {
   return (
 
     <div
-    className='bg-[#1C1917]  w-fit mx-auto px-2 md:px-12 py-2 rounded-lg '
+    className='bg-[#1C1917]  w-fit mx-auto px-2 md:px-12 py-1 sm:py-2 rounded-lg '
         >
             {/* someting */}
 
-        <ul className='flex flex-row flex-shrink space-x-12 text-xs md:text-lg '>
+        <ul className='flex flex-row flex-shrink space-x-12 text-xs md:text-lg px-2'>
             {
                 navSection.map((item,index)=>{
                     return(
                         <li 
                         key={index}
-                        className='text-white hover:text-[#6B7280] font-semibold 
+                        className='text-white hover:text-[#6B7280] font-normal md:font-semibold 
                         transition-all duration-300  hover:scale-110 ease-in-out
                         text-md hover:cursor-pointer'>
                         {item}
