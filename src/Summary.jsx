18f82f40370d489eb6b44ed005492e80f0b3e4ee
antonnprince <antonprince95@gmail.com>
2025-04-27 10:35:52 +0530

    tech stack done

--- src/Summary.jsx
+++ src/Summary.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Summary = () => {
   return (
-            <div className='flex flex-col sm:flex-row space-y-2 justify-between text-2xl md:text-3xl lg:text-5xl font-bold mt-8 md:mt-12'>
+            <div className='flex flex-col sm:flex-row space-y-2
+             justify-between text-2xl md:text-3xl lg:text-5xl font-bold mt-8 md:mt-12'>
                 <h1> <span className='text-[#6B7280]'>+5</span> PROJECTS</h1>
                 <h1><span className='text-[#6B7280]'>+3</span> INTERNSHIPS</h1>
                 <h1><span className='text-[#6B7280]'>+7</span> HACKATHONS</h1>
--- src/TechStacks.jsx
+++ src/TechStacks.jsx
@@ -11,24 +11,25 @@ import huggingface from "./assets/huggingface.svg"
 const TechStacks = () => {
   return (
     <div 
-   className='flex flex-col h-fit'
+   className='flex mt-8 md:mt-12 flex-col h-fit'
     >
     
         <h1 
-        className='my-12 mx-auto text-[#6B7280] text-2xl md:text-3xl lg:text-6xl font-extrabold'>
+        className='my-1 md:my-4 mx-auto text-[#6B7280] text-2xl md:text-3xl lg:text-6xl font-extrabold'>
             TECH STACKS</h1>
 
        
-            <div className='flex flex-row mx-auto w-5/6 justify-between '>
-              <img src={react} className='w-16 h-16 '/>
-              <img src={mongo} className='w-16 h-16  scale-250'/>
-              <img src={express} className='w-16 h-16 '/>
-              <img src={node} className='w-16 h-16 '/>
-              <img src={firebase} className='w-16 h-16 '/>
-              <img src={supabase} className='w-16 h-16 '/>
-              <img src={fastapi} className='w-16 h-16 '/>
-              <img src={huggingface} className='w-16 h-16 bg-white rounded-full '/>
+            <div className='flex flex-row space-x-4 mx-auto w-full justify-between p-2 overflow-x-auto overflow-y-hidden'>
+              <img src={react} className='w-8 h-8 md:w-16 md:h-16 inline-block' />
+              <img src={mongo} className='w-8 h-8 md:w-16 md:h-16 scale-170  inline-block' />
+              <img src={express} className='w-8 h-8 md:w-16 md:h-16 inline-block' />
+              <img src={node} className='w-8 h-8 md:w-16 md:h-16 inline-block' />
+              <img src={firebase} className='w-8 h-8 md:w-16 md:h-16 inline-block' />
+              <img src={supabase} className='w-8 h-8 md:w-16 md:h-16 inline-block' />
+              <img src={fastapi} className='w-8 h-8 md:w-16 md:h-16 inline-block' />
+              <img src={huggingface} className='w-8 h-8 md:w-16 md:h-16 bg-white rounded-full inline-block' />
             </div>
+
     </div>
   )
 }
