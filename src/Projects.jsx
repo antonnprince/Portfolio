66fb207e30400e98b58303754aab1ea788c3ce08
antonnprince <antonprince95@gmail.com>
2025-05-03 13:03:59 +0530

    add project

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -3,23 +3,49 @@ import React from 'react'
 const Projects = () => {
   const projects = [
     {
-        title: "HeartLabs",
-        description: "Mobile Application built using React Native and Supabase that helps doctors find clinical postings.",
-        link: "",
-        image: "https://i.imgur.com/4v1X2xk.png"
+        title: "Hyperly",
+        description: "This is a Saas platform where people can generate industry and product relevant content for their social media platforms, increasing social media presence and saving time",
+        link: "hyperlyinternship.netlify.app/",
+        // image: "https://ephemeral-treacle-2f546e.netlify.app/"
     },
     {
-        title: "TechMaghi",
-        description: "Developed departmental dashboard that helps employees to manage their tasks and projects.",
-        link: "",
-        image: "https://i.imgur.com/4v1X2xk.png"
+        title: "Elite Arena",
+        description: "Esports website that allows users to register and host tournaments.",
+        link:""
+        // image: "https://i.imgur.com/4v1X2xk.png"
     },
     {
-        title: "Raftel Tech",
-        description: "Developed a website using HTML, CSS, and JavaScript.",
-        link: "",
-        image: "https://i.imgur.com/4v1X2xk.png"
-    }
+      title: "Legal Lens",
+      description: "A website that allows legal professionals to get strategies and predictive insights about their current cases",
+      link: "",
+            // image: "https://i.imgur.com/4v1X2xk.png"
+    },
+    {
+      title:"Youtube Video Sentiment Analysis",
+      description:"A website that performs sentiment analysis on a youtube video's comments when a valid URL is provided as input. The output is shown as a graph and is divided into 3 parts, positive neutral and negative",
+      link:"sentiment-analysis-lac.vercel.app",
+    },
+    {
+      title:"FortiFi",
+      description:"Customizable guardrail system for LLMs that allows organizations to implement domain-specific rules, which is decentralized and can be viewewd by everyone",
+      link:"",
+    },
+    {
+      title:"Campus Lift",
+      description:"Ride sharing app developed for students of our college, as part of our mini project.",
+      link:""
+    },
+    {
+      title:"CSV Filter",
+      description:"A website that takes csv files as input and gives out csv with duplicate values as outputs in fields specified by user",
+      link:""
+    },
+    {
+      title:"CSV Filter",
+      description:"A website that takes csv files as input and gives out csv with duplicate values as outputs in fields specified by user",
+      link:""
+    },
+    
   ]
 
   return (
@@ -28,14 +54,14 @@ const Projects = () => {
     >
        <h1 className='my-1 md:my-4 mx-auto text-[#6B7280] text-center  text-4xl md:text-3xl lg:text-7xl font-extrabold'
         >PROJECTS</h1>
-        <div className='flex flex-row space-x-4 w-full justify-between mt-12 space-y-12 p-0'>
+        <div className='flex flex-row flex-wrap space-x-4 w-full justify-between mt-12 space-y-12 p-0'>
       {
         projects.map((project, index) => (
-            <div key={index} className='w-full md:w-3/4 mx-auto mb-8 '>
+            <div key={index} className='w-full md:w-1/4 mx-auto mb-8 '>
                 <h3 className='text-2xl md:text-2xl lg:text-3xl font-extrabold text-[#6B7280]'
                 //  style={{ textShadow: '3px 3px 7px rgba(255, 255, 255, 0.17)' }}
                >{index + 1}. {project.title.toUpperCase()}</h3> 
-                <p className='text-sm md:text-lg font-bold'>{project.description}</p>
+                <p className='text-sm md:text-lg font-thin'>{project.description}</p>
                 <img src={project.image} alt={project.title} className='w-full h-auto' />
             </div>
         ))
