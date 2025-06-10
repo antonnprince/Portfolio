4aa135ac120db345a6ba192eadcc4591586e84d5
antonnprince <antonprince95@gmail.com>
2025-06-10 11:19:04 +0530

    projects link update

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import link from "./assets/link.svg"
-
+import { Link } from 'react-router-dom'
 
 const Projects = () => {
   const projects = [
     {
         title: "Hyperly",
         description: "This is a Saas platform where people can generate industry and product relevant content for their social media platforms, increasing social media presence and saving time",
-        link: "hyperlyinternship.netlify.app/",
+        link: "https://hyperlyinternship.netlify.app/",
     },
     {
         title: "Elite Arena",
@@ -16,13 +16,18 @@ const Projects = () => {
     },
     {
       title: "Legal Lens",
-      description: "A website that allows legal professionals to get strategies and predictive insights about their current cases",
-      link: "",
+      description: "A website that allows legal professionals to get strategies and predictive insights about their current cases. I have used a Llama-3.2-3b model fine tuned on custom dataset to generate accurate arguments.",
+      link: "https://github.com/antonnprince/Legal-Lens-Web-UI",
     },
     {
       title:"Sentiment Analysis",
       description:"A website that performs sentiment analysis on a youtube video's comments when a valid URL is provided as input. The output is shown as a graph and is divided into 3 parts, positive neutral and negative",
-      link:"sentiment-analysis-lac.vercel.app",
+      link:"https://sentiment-analysis-lac.vercel.app",
+    },
+    {
+      title:"Streak/Task Management Application",
+      description:"A react native application built using expo sdk that keeps track of user's daily,weekly and monthly goals, and maintains streaks.",
+      link:"https://github.com/antonnprince/Streak-Counter",
     },
     {
       title:"FortiFi",
@@ -55,11 +60,11 @@ const Projects = () => {
                 <h3 className='text-2xl md:text-2xl lg:text-3xl font-extrabold  my-1 sm:my-2 md:my-4 text-[#6B7280]'
                >{index + 1}. {project.title.toUpperCase()}</h3> 
                 <p className='text-sm md:text-lg font-thin text-center w-3/4 md:w-full mx-auto'>{project.description}</p>             
-               
-                <h3 className='text-sm md:text-lg text-center font-thin text-blue-500'>
-                  {project.link}
+                  {/* <Link to={project.link}> */}
+                    <h3 className='text-sm md:text-lg text-center font-thin text-blue-500'>
+                    <a href={project.link}>{project.link}</a>
                 </h3>
-            
+             {/* </Link>   */}
             </div>
         ))
       }
